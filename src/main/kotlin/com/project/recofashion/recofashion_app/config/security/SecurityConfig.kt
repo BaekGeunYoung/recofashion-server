1bb0822ccaadd55b04129104a316fdd96c5815dc
BaekGeunYoung <dvmflstm@naver.com>
2019-11-13 17:06:46 +0900

    make configuration files

---
+++ src/main/kotlin/com/project/recofashion/recofashion_app/config/security/SecurityConfig.kt
@@ -0,0 +1,45 @@
+package com.project.recofashion.recofashion_app.config.security
+
+import com.project.recofashion.recofashion_app.config.security.jwt.JwtConfig
+import com.project.recofashion.recofashion_app.config.security.jwt.JwtTokenProvider
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+        @Autowired private val userDetailsService: UserDetailsService,
+        @Autowired private val bCryptPasswordEncoder: BCryptPasswordEncoder,
+        @Autowired private val jwtTokenProvider: JwtTokenProvider
+) : WebSecurityConfigurerAdapter()
+{
+    @Bean
+    override fun authenticationManagerBean(): AuthenticationManager {
+        return super.authenticationManagerBean()
+    }
+
+    override fun configure(http: HttpSecurity) {
+        http
+                .csrf().disable()
+                .antMatcher("/api/**").authorizeRequests()
+                .antMatchers("/api/v1/user/register").anonymous()
+                .antMatchers("/api/v1/user/login").anonymous()
+                .anyRequest().authenticated()
+                .and()
+                .apply(JwtConfig(jwtTokenProvider))
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder)
+    }
+}
