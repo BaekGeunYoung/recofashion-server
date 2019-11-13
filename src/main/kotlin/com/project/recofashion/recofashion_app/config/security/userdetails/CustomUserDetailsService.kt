de757dbe0037743e09146f908210211ce0116223
BaekGeunYoung <dvmflstm@naver.com>
2019-11-13 17:35:53 +0900

    configuring custom UserDetails & UserDetailsService

---
+++ src/main/kotlin/com/project/recofashion/recofashion_app/config/security/userdetails/CustomUserDetailsService.kt
@@ -0,0 +1,18 @@
+package com.project.recofashion.recofashion_app.config.security.userdetails
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class CustomUserDetailsService(@Autowired private val userRepository: UserRepository): UserDetailsService
+{
+    override fun loadUserByUsername(username: String?): UserDetails {
+        val user = userRepository.findByUsername(username)
+        user?: throw UsernameNotFoundException("cannot find such username: $username")
+
+        return CustomUserDetails(user)
+    }
+}
