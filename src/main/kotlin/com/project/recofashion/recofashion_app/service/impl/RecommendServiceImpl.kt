acbc33881fb3f8f404a89c619b485bb4e1c4ce88
BaekGeunYoung <dvmflstm@naver.com>
2019-11-16 23:50:13 +0900

    made structure of recommendation procesS

--- src/main/kotlin/com/project/recofashion/recofashion_app/service/impl/RecommendServiceImpl.kt
+++ src/main/kotlin/com/project/recofashion/recofashion_app/service/impl/RecommendServiceImpl.kt
@@ -1,11 +1,13 @@
 package com.project.recofashion.recofashion_app.service.impl
 
 import com.project.recofashion.recofashion_app.entity.user.Color
+import com.project.recofashion.recofashion_app.entity.user.User
 import com.project.recofashion.recofashion_app.repository.HistoryRepository
 import com.project.recofashion.recofashion_app.repository.UserRepository
 import com.project.recofashion.recofashion_app.service.RecommendService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 
 @Service
@@ -15,30 +17,35 @@ class RecommendServiceImpl(
 ) : RecommendService {
     private val SHORT = "short"
     private val SHORT_SLEEVE = "short sleeve"
-    private val SLEEVELESS = "short sleeve"
-    private val COTTON_PANTS = "short sleeve"
-    private val JEANS = "short sleeve"
-    private val CARDIGAN = "short sleeve"
-    private val LONG_SLEEVE = "short sleeve"
-    private val KNIT = "short sleeve"
-    private val JACKET = "short sleeve"
-    private val JUMPER = "short sleeve"
+    private val SLEEVELESS = "shortless"
+    private val COTTON_PANTS = "cotton pants"
+    private val JEANS = "jeans"
+    private val CARDIGAN = "cardigan"
+    private val LONG_SLEEVE = "long sleeve"
+    private val KNIT = "knit"
+    private val JACKET = "jacket"
+    private val JUMPER = "jumper"
     private val TRENCH_COAT = "trench coat"
     private val COAT = "coat"
     private val LEATHER_JACKET = "leather jacket"
     private val HITTEK = "hittek"
-    private val PADDIGN = "padding"
+    private val PADDING = "padding"
     private val SCARF = "scarf"
     private val FLEECE_LINED = "fleece-lined"
 
     override fun recommend(temperature: Int, userDetails: UserDetails): MutableMap<String, Any> {
+        val ret: MutableMap<String, Any> = HashMap()
         val user = userRepository.findByUsername(userDetails.username)
 
+        user?: throw UsernameNotFoundException("cannot find such user : ${userDetails.username}")
+
+        ret["clothes"] = recommendClothes(temperature)
+        ret["colors"] = recommendColors(temperature, user)
     }
     
     fun recommendClothes(temperature: Int) : List<String> {
         return listOf("jacket", "padding", "coat")
     }
 
-    fun recommendColors() : List<Color> = listOf(Color(255, 255, 0))
+    fun recommendColors(temperature: Int, user: User) : List<Color> = listOf(Color(255, 255, 0))
 }
