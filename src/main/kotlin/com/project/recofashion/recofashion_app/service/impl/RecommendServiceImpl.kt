d540bab3f7757c00ed8c308343cf0d985a1adb74
BaekGeunYoung <dvmflstm@naver.com>
2019-11-17 00:03:17 +0900

    make clothes recommendation function

--- src/main/kotlin/com/project/recofashion/recofashion_app/service/impl/RecommendServiceImpl.kt
+++ src/main/kotlin/com/project/recofashion/recofashion_app/service/impl/RecommendServiceImpl.kt
@@ -17,7 +17,9 @@ class RecommendServiceImpl(
 ) : RecommendService {
     private val SHORT = "short"
     private val SHORT_SLEEVE = "short sleeve"
-    private val SLEEVELESS = "shortless"
+    private val SLEEVELESS = "sleeveless"
+    private val SLACKS = "slacks"
+    private val SHIRTS = "shirts"
     private val COTTON_PANTS = "cotton pants"
     private val JEANS = "jeans"
     private val CARDIGAN = "cardigan"
@@ -31,7 +33,8 @@ class RecommendServiceImpl(
     private val HITTEK = "hittek"
     private val PADDING = "padding"
     private val SCARF = "scarf"
-    private val FLEECE_LINED = "fleece-lined"
+    private val HOOD = "hood"
+    private val MAN_TO_MAN = "man to man"
 
     override fun recommend(temperature: Int, userDetails: UserDetails): MutableMap<String, Any> {
         val ret: MutableMap<String, Any> = HashMap()
@@ -41,10 +44,19 @@ class RecommendServiceImpl(
 
         ret["clothes"] = recommendClothes(temperature)
         ret["colors"] = recommendColors(temperature, user)
+
+        return ret
     }
     
     fun recommendClothes(temperature: Int) : List<String> {
-        return listOf("jacket", "padding", "coat")
+        if(temperature >= 27) return listOf(SHORT, SHORT_SLEEVE, SLEEVELESS)
+        if(temperature >= 23) return listOf(SHORT, SHORT_SLEEVE, COTTON_PANTS)
+        if(temperature >= 20) return listOf(LONG_SLEEVE, CARDIGAN, HOOD, COTTON_PANTS, SLACKS)
+        if(temperature >= 17) return listOf(SHIRTS, MAN_TO_MAN, CARDIGAN, HOOD, JEANS, COTTON_PANTS, SLACKS)
+        if(temperature >= 12) return listOf(KNIT, SHIRTS, MAN_TO_MAN, CARDIGAN, HOOD, JACKET, JEANS)
+        if(temperature >= 10) return listOf(KNIT, JUMPER, MAN_TO_MAN, HOOD, LEATHER_JACKET, TRENCH_COAT)
+        if(temperature >= 6) return listOf(COAT, LEATHER_JACKET, JUMPER, HITTEK)
+        return listOf(HITTEK, COAT, PADDING, SCARF)
     }
 
     fun recommendColors(temperature: Int, user: User) : List<Color> = listOf(Color(255, 255, 0))
