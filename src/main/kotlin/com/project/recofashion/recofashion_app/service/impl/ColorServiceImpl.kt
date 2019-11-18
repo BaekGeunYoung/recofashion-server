8756add675f55b85454941f31159674aaf1bfed5
BaekGeunYoung <dvmflstm@naver.com>
2019-11-18 11:55:55 +0900

    임의의 색상을 비비드 톤/모노 톤으로 변환하는 함수 작성

--- src/main/kotlin/com/project/recofashion/recofashion_app/service/impl/ColorServiceImpl.kt
+++ src/main/kotlin/com/project/recofashion/recofashion_app/service/impl/ColorServiceImpl.kt
@@ -16,13 +16,51 @@ class ColorServiceImpl : ColorService {
 
     /*주어진 점으로부터 x=y=z 직선에 가장 가까운 점을 구한다.*/
     override fun getMonoColor(color: Color): Color {
-
+        /*우리가 원하는 점을 벡터 V라 하고, 주어진 점(a,b,c)을 벡터 w라 하면, (v-w)와 (1,1,1)을 내적한 값이 0이 되어야 한다.
+        * 이 조건을 만족하는 벡터 v의 x,y,z 값은 (a+b+c)/3이다. */
+        val monoValue = (color.r + color.g + color.b) / 3
+        return Color(monoValue, monoValue, monoValue)
     }
 
-    /*주어진 점의 원색을 구한다.*/
+    /*주어진 점의 원색을 구한다.
+    * 주어진 점에서 rgb boundary 정육면체의 6개 모서리 중 가장 가까운 모서리에 내린 수선의 발을 구한다.
+    * */
     override fun getVividColor(color: Color): Color {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val x = color.r
+        val y = color.g
+        val z = color.b
+
+        return if(x < y) {
+            if(255 - y < x) {
+                if(x > z) greenYellowPerpendicular(color)
+                else greenSkyPerpendicular(color)
+            } else {
+                if(z < y) greenSkyPerpendicular(color)
+                else blueSkyPerpendicular(color)
+            }
+        }
+        else {
+            if(255 - x > y) {
+                if(z > x) bluePurplePerpendicular(color)
+                else redPurplePerpendicular(color)
+            } else {
+                if(y < z) redPurplePerpendicular(color)
+                else redYellowPerpendicular(color)
+            }
+        }
     }
+
+    fun redPurplePerpendicular(color: Color) : Color = Color(255, 0, color.b)
+
+    fun redYellowPerpendicular(color: Color): Color = Color(255, color.g, 0)
+
+    fun greenYellowPerpendicular(color: Color): Color = Color(color.r, 255, 0)
+
+    fun greenSkyPerpendicular(color: Color): Color = Color(0, 255, color.b)
+
+    fun blueSkyPerpendicular(color: Color): Color = Color(0, color.g, 255)
+
+    fun bluePurplePerpendicular(color: Color): Color = Color(color.r, 0, 255)
 
     /*1. 주어진 점과 점(255,255,255)를 지나는 직선을 구한다.
     * 2. 이 직선이 rgb boundary와 접하는 점을 구한다.
