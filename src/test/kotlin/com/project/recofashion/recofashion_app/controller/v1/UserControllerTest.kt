da8882ac1a29fae39228844cd4f718cb97d05d78
BaekGeunYoung <dvmflstm@naver.com>
2019-11-19 18:35:13 +0900

    delete unused symbols

--- src/test/kotlin/com/project/recofashion/recofashion_app/controller/v1/UserControllerTest.kt
+++ src/test/kotlin/com/project/recofashion/recofashion_app/controller/v1/UserControllerTest.kt
@@ -8,13 +8,11 @@ import com.project.recofashion.recofashion_app.entity.user.Color
 import com.project.recofashion.recofashion_app.entity.user.Role
 import com.project.recofashion.recofashion_app.entity.user.Skin
 import com.project.recofashion.recofashion_app.entity.user.User
-import com.project.recofashion.recofashion_app.service.UserService
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
