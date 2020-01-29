adf519843554ccef06dce1e5f2e6567ecf01c82b
BaekGeunYoung <dvmflstm@naver.com>
2020-01-29 17:59:59 +0900

    modify CORS Policy config : make deployHost env var

--- src/main/kotlin/com/project/recofashion/recofashion_app/config/web/WebConfig.kt
+++ src/main/kotlin/com/project/recofashion/recofashion_app/config/web/WebConfig.kt
@@ -1,14 +1,18 @@
 package com.project.recofashion.recofashion_app.config.web
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig: WebMvcConfigurer {
+class WebConfig : WebMvcConfigurer {
+    @Value("\${DEPLOY_HOST}")
+    private val deployHost: String = ""
+
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://52.79.179.196")
+                .allowedOrigins("http://localhost:3000", deployHost)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("authorization", "content-type")
                 .exposedHeaders("authorization")
